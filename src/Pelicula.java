7bcb3ba26fbc2e2299af8f6fe88b4f5caa62f471
sherlons <lukasteoo03@gmail.com>
2024-10-22 10:19:29 -0500

    programa

---
+++ src/Pelicula.java
@@ -0,0 +1,7 @@
+public record Pelicula(int episode_id,
+                       String title,
+                       String opening_crawl,
+                       String director,
+                       String producer,
+                       String release_date) {
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+
+
+            var consulta = new ConsultaPelicula();
+            System.out.println("QUE PELICULA QUIERES CONSULTAR");
+            try {
+                var numeroDePelicula = Integer.valueOf(lectura.nextLine());
+                Pelicula pelicula = consulta.buscarPelicula(numeroDePelicula);
+                System.out.println(pelicula);
+                GeneradorDeArchivo generador = new GeneradorDeArchivo();
+                generador.guardarJson(pelicula);
+            }catch (NumberFormatException | IOException e){
+                System.out.println("Numero no encontrado"+e.getMessage());
+            }catch (RuntimeException e){
+                System.out.println(e.getMessage());
+                System.out.println("Finalizo el programa");
+            }
+
+    }
 }
